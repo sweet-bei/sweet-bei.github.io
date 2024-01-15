3473176c6a80c2f8a470bda5e08006fca914a6dd
sweet-bei <1847959118@qq.com>
2024-01-15 20:02:10 +0800

    新增JS文件，点击切换背景颜色

--- js/common.js
+++ js/common.js
@@ -1,6 +1,14 @@
-var bgc_style = document.getElementById("bgc_style");
-bgc_style.onclick = function () {
-    document.body.style.backgroundColor = "#000"
-    document.body.style.color = "#fff"
-    this.className = "iconfont size icon-yueliang"
-};
+const clickLi = document.querySelector('.nav ul li:last-child')
+let flag = false
+
+clickLi.addEventListener('click', function () {
+    // console.log(111)
+    if (!flag) {
+        document.body.style.backgroundColor = '#000'
+        document.body.style.color = '#fff'
+    } else {
+        document.body.style.backgroundColor = ''
+        document.body.style.color = ''
+    }
+    flag = !flag
+})
